f5fc452d591887ec05395bc0e1955b4d6e4380a4
Fred Belisario <fred.belisario@gmail.com>
2020-03-25 17:43:13 +0000

    adicionando projeto config server

---
+++ Treinamento_ConfigServer/src/main/java/br/com/becb/TreinamentoConfigServerApplication.java
@@ -0,0 +1,14 @@
+package br.com.becb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class TreinamentoConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinamentoConfigServerApplication.class, args);
+	}
+
+}
---
+++ Treinamento_ConfigServer/src/test/java/br/com/becb/TreinamentoConfigServerApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.becb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TreinamentoConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
