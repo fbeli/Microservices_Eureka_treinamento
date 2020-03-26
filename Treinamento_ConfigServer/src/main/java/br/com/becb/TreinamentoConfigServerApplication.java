5fdc7ba6e42ac05a62493019b74b05529bda55fc
Fred Belisario <fred.belisario@gmail.com>
2020-03-26 10:50:56 +0000

    adicionando configurações no API gateway para acesso ao config server

--- Treinamento_ConfigServer/src/main/java/br/com/becb/TreinamentoConfigServerApplication.java
+++ Treinamento_ConfigServer/src/main/java/br/com/becb/TreinamentoConfigServerApplication.java
@@ -2,9 +2,10 @@ package br.com.becb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-
+@EnableConfigServer
 public class TreinamentoConfigServerApplication {
 
 	public static void main(String[] args) {
